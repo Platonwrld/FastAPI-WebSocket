c4e8dfc63587a68cb39f981ecba0f45e33a9b3d9
Ваше Имя <platon888est@protonmail.ru>
2022-07-29 15:00:39 +0300

    eighth

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 import json
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory='templates')
@@ -17,11 +18,11 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     id = 1
     while True:
-        data = await websocket.receive_json()
-        await websocket.send_json(f"{id}. {data}")
-        id += 1
-        
-
+        data_1 = await websocket.receive_json()
+        data_2 = {}
+        data_2[id] = data_1['id']
+        data_2 = json.dumps(data_2)
+        await websocket.send_json(data_2)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, host='127.0.0.1', reload=True)
